35034b333a49e6e0b75c5eddb0005bba2bb58eef
muhfakhri <nurfakhri445@gmail.com>
2026-09-22 20:09:40 +0700

    studikasus 3

--- perlulangan/studi_kasus_3.cpp
+++ perlulangan/studi_kasus_3.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 int main() {
+    long long total = 0;
+    int hari = 0;
+    long long target = 2000000;
+    long long tabunganHarian = 10000;
+    long long tabunganHariKe7 = 50000;
 
-    int hari,tabungan,target,setoran_hari;
+    
+    do {
+        hari++;
 
-    target = 2000000;
-    tabungan = 0;
-    hari = 0;
-    setoran_hari = 0;
+        if (hari % 7 == 0) {
+            total += tabunganHariKe7;
+        } else {
+            total += tabunganHarian;
+        }
 
+    } while (total < target);
+
+    cout << "Jumlah hari yang dibutuhkan: " << hari << " hari" << endl;
+    cout << "Total tabungan akhir: Rp" << total << endl;
+
+    if (total == target) {
+        cout << "Target tercapai TEPAT pada hari ke-" << hari << endl;
+    } else {
+        cout << "Target TERLEWATI pada hari ke-" << hari << endl;
+        cout << "Kelebihan: Rp" << (total - target) << endl;
+    }
 
     return 0;
 }
